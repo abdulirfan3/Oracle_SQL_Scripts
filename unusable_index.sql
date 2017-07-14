cebc76ca82fca27d322a112ae5d2f18d51949c76
xi Wang <vincent_wangxi@126.com>
2017-07-14 16:16:06 +0800

    add order by owner table
    add order by owner table

--- unusable_index.sql
+++ unusable_index.sql
@@ -3,11 +3,13 @@ col index_name format a30
 col table_name format a30
 col index_type format a10
 col table_owner format a10
- 
- 
+col status format a10
+
+
  ACCEPT owner prompt 'Enter owner name or hit enter to search all user : '
- select di.OWNER, di.INDEX_NAME, di.INDEX_TYPE, di.TABLE_OWNER, di.TABLE_NAME, di.LAST_ANALYZED, di.PARTITIONED, di.STATUS, ds.bytes/1024/1024 "size in MB"  
+ select di.OWNER, di.INDEX_NAME, di.INDEX_TYPE, di.TABLE_OWNER, di.TABLE_NAME, di.LAST_ANALYZED, di.PARTITIONED, di.STATUS, ds.bytes/1024/1024 "size in MB"
  from DBA_indexes di, dba_segments ds
   WHERE di.index_name=ds.segment_name
   and di.owner like upper(nvl('%&owner%',di.owner))
-  and STATUS = 'UNUSABLE';
+  and STATUS = 'UNUSABLE'
+  order by di.OWNER, di.INDEX_NAME;
